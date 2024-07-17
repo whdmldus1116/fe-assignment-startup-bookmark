4fe40f6db6e499e3b56f47d3d1d604a04bd1420d
yoouung <jyp101400@gmail.com>
2024-07-17 15:35:12 +0900

    Style: reset 스타일 적용
    Signed-off-by: yoouung <jyp101400@gmail.com>

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import normalize from 'emotion-normalize';
-import { css, Global } from '@emotion/react';
+import { Global } from '@emotion/react';
 import GlobalStyles from './globalStyles';
+import { reset } from './styles/reset';
 
 //
 import App from './App';
@@ -11,6 +12,7 @@ import initializeMockupWorker from '@/__mock__';
 initializeMockupWorker().then(() => {
   ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
+      <Global styles={reset} />
       <GlobalStyles />
       <App />
     </React.StrictMode>,
