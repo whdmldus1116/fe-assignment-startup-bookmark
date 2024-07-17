51796b090e857e087ec68b2736dbd977714a6fcf
whdmldus1116 <76983867+whdmldus1116@users.noreply.github.com>
2024-07-18 00:31:27 +0900

    Refactor : 라우터에 Login페이지 생성

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,11 @@
 import { Mobile, PC } from './MediaQuery';
-import './App.css';
-
-import Header from './components/header';
 import Router from './Router';
 
 const App = () => {
-  const currentPath = window.location.pathname;
-  const isLoggedIn = true; // test
-  const username = '유니콘'; // test
-
   return (
     <>
       <Mobile>이건 모바일 !!</Mobile>
       <PC>
-        <Header isLoggedIn={isLoggedIn} currentPath={currentPath} username={username} />
         <Router />
       </PC>
     </>
