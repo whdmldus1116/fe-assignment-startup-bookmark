fa2200db374d096b1f6887b9128caf261d403b1d
yoouung <jyp101400@gmail.com>
2024-07-18 10:12:14 +0900

    Feat: SignUp 페이지 생성
    - 회원가입 페이지 생성
    - Title 컴포넌트 생성 및 적용
    - utils 디렉토리 추가
    - textInput에 릴리 도움 요청 ,,
    
    close #17
    
    Signed-off-by: yoouung <jyp101400@gmail.com>

--- src/components/textInput/index.tsx
+++ src/components/textInput/index.tsx
@@ -16,6 +16,12 @@ const TextInput = forwardRef<HTMLInputElement, Props>(
     const [text, setText] = useState(value);
 
     const handleChange = (ev: ChangeEvent<HTMLInputElement>) => {
+      /* TODO: 릴리 도와주세요 ㅜㅜ
+       * 회원가입 폼 컴포넌트에서 전화번호 입력 시에는 '-'이 들어간 string을 value로 전달해주는데,
+       * 여기서 value를 다시 e.target.value로 받아서 setText(value)를 하면 숫자만 value로 설정되느라 '-'이 사라지는 것 같습니다ㅠㅠㅠ
+       * 어떻게 수정해야 할지 모르겠어요 ,,,
+       * */
+
       const value = ev.target.value;
       setText(value);
       onChange({ value });
