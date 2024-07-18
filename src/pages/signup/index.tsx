0330b71b1dcc36f6a96b04556523888304f4da61
yoouung <jyp101400@gmail.com>
2024-07-18 11:44:47 +0900

    Refactor: 스크린에 Header 추가
    - rename: pages/signup.tsx -> pages/signup/index.tsx

---
+++ src/pages/signup/index.tsx
@@ -0,0 +1,29 @@
+import styled from 'styled-components';
+import LineText from '../../components/lineText';
+import { TitleStyle } from '../../components/title/styles';
+import SignUpForm from '../../components/signUpForm';
+import Header from '../../components/header';
+
+const SignUpScreen = () => {
+  return (
+    <>
+      <Header isLoggedIn={false} currentPath="/signup" />
+      <SignUpContainer>
+        <TitleStyle>회원가입</TitleStyle>
+        <LineText text="이미 계정이 있으신가요?" textLine={true} />
+
+        <SignUpForm />
+      </SignUpContainer>
+    </>
+  );
+};
+
+const SignUpContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  margin-top: 105px;
+  align-items: center;
+`;
+
+export default SignUpScreen;
