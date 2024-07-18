71afb71395d3b2e441d2f7561217b583c91ad95f
yoouung <jyp101400@gmail.com>
2024-07-18 11:44:47 +0900

    Refactor: 스크린에 Header 추가
    - rename: pages/signup.tsx -> pages/signup/index.tsx

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,13 +2,13 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MenuPage from './pages/menu';
 import LoginScreen from './pages/login';
-import SignUpPage from './pages/signup';
+import SignUpScreen from './pages/signup';
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/signup" element={<SignUpScreen />} />
         <Route path="/menu" element={<MenuPage />} />
         <Route path="/login" element={<LoginScreen />} />
       </Routes>
