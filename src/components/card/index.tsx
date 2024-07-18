5a61746bfd4379eefc144ae703f4bf09cb8ca125
whdmldus1116 <76983867+whdmldus1116@users.noreply.github.com>
2024-07-18 11:34:02 +0900

    Feat : 북마크 클릭시 상태변화

--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   CardWrapper,
   Image,
@@ -25,6 +25,12 @@ const Card = ({
   description = '주식회사 하프스는 빅데이터 기술을 기반으로 스타트업과 투자자 연결하는 온라인 플...',
   bookmark,
 }: Props) => {
+  const [isBookmarked, setIsBookmarked] = useState(bookmark);
+
+  const handleBookmarkClick = () => {
+    setIsBookmarked(!isBookmarked);
+  };
+
   return (
     <CardWrapper>
       <Tag>{tag}</Tag>
@@ -32,7 +38,12 @@ const Card = ({
       <ContentWrapper>
         <Title>{id}파트너스</Title>
         <Description>{description}</Description>
-        <BookmarkIcon src={bookmark ? bookMark : bookMarkNone} alt="Bookmark" />
+        <BookmarkIcon
+          src={isBookmarked ? bookMark : bookMarkNone}
+          alt="Bookmark"
+          onClick={handleBookmarkClick}
+          style={{ cursor: 'pointer' }}
+        />
       </ContentWrapper>
     </CardWrapper>
   );
