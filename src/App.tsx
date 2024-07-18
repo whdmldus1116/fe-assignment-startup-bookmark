6dc95445c2079382e8fff7f13dc1d82e28ce2ab9
whdmldus1116 <76983867+whdmldus1116@users.noreply.github.com>
2024-07-18 14:44:18 +0900

    Feat: 북마크 스크린 생성

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
+import Card from './components/card';
+import DropUp from './components/dropUp';
 import { Mobile, PC } from './MediaQuery';
 import Router from './Router';
 
 const App = () => {
+  const handleChange = (selectedOptions: string[]) => {
+    console.log('Selected options:', selectedOptions);
+  };
   return (
     <>
       <Mobile>이건 모바일 !!</Mobile>
       <PC>
+        <Card id="01" bookmark={true} />
+        <Card id="02" bookmark={false} />
+        <DropUp onChange={handleChange} />
         <Router />
       </PC>
     </>
