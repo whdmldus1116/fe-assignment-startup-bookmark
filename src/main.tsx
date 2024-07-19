50c85741840c907389a07bf5a541039300f8d9a6
yoouung <jyp101400@gmail.com>
2024-07-19 22:06:02 +0900

    Style: reset.tsx와 gloablStyle.tsx 병합

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import normalize from 'emotion-normalize';
-import { Global } from '@emotion/react';
 import GlobalStyles from './styles/globalStyles';
-import { reset } from './styles/reset';
 
 import App from './App';
 import initializeMockupWorker from '@/__mock__';
@@ -11,7 +8,6 @@ import initializeMockupWorker from '@/__mock__';
 initializeMockupWorker().then(() => {
   ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
-      <Global styles={reset} />
       <GlobalStyles />
       <App />
     </React.StrictMode>,
